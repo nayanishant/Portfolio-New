70589b89688f161c84aff7535c7b3f944a6e323a
nyanishant <nayanishant.info@gmail.com>
2023-05-04 00:39:22 +0530

    added dblclick to hide sidebar

--- script.js
+++ script.js
@@ -3,5 +3,15 @@ $(document).ready(function () {
     $('#line').on('click', function () {
         $('aside').toggleClass('visible')
     })
+
+    $(window).on('dblclick', function () {
+
+        var asideHasClass = $('aside').hasClass('visible')
+
+        if (asideHasClass) {
+            $('aside').removeClass('visible')
+        }
+        
+    })
     
 })
