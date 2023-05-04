418042b333f3162575fd9701873a7f0a9012e337
nyanishant <nayanishant.info@gmail.com>
2023-05-04 23:54:46 +0530

    added glass effect and onclick to hide sidebar

--- script.js
+++ script.js
@@ -4,6 +4,10 @@ $(document).ready(function () {
         $('aside').toggleClass('visible')
     })
 
+    $('ul li a').on('click',function () {
+        $('aside').removeClass('visible')
+    })
+
     $(window).on('dblclick', function () {
 
         var asideHasClass = $('aside').hasClass('visible')
