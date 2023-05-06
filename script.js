3c67a023f76b80642be6c1e0b24ab81b9b782e6d
nyanishant <nayanishant.info@gmail.com>
2023-05-07 00:27:10 +0530

    fixed colors

--- script.js
+++ script.js
@@ -18,4 +18,36 @@ $(document).ready(function () {
         
     })
     
+    // var projectData = [
+    //     {
+    //         "id": 1,
+    //         "name": "SHeepyShops",
+    //         "url": "https://github.com/nayanishant/SheepyShops",
+    //         "image": "Gallery/SheepyShops.png"
+    //     },
+    //     {
+    //         "id": 2,
+    //         "name": "HackClub",
+    //         "url": "https://github.com/Gautam-flash/hc_pesce_site",
+    //         "image": "Gallery/Hack Club.png"
+    //     },
+    //     {
+    //         "id": 3,
+    //         "name": "Portfolio",
+    //         "url": "https://github.com/nayanishant/Portfolio",
+    //         "image": "Gallery/Portfolio.png"
+    //     },
+    //     {
+    //         "id": 4,
+    //         "name": "IoT Mini Project",
+    //         "url": "https://github.com/nayanishant/iot",
+    //         "image": "Gallery/IoTMini.png"
+    //     },
+    //     {
+    //         "id": 5,
+    //         "name": "Blog Website",
+    //         "url": "https://github.com/nayanishant/peace-hub",
+    //         "image": "Gallery/pesce.png"
+    //     }
+    // ]
 })
