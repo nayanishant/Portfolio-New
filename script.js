eb18e7de167457e3e62d72fb26b4474702e0a680
NishantSwat <nishant@swatalliance.com>
2024-01-08 15:04:20 +0530

    added experience and tools

--- script.js
+++ script.js
@@ -9,4 +9,42 @@ $(document).ready(function () {
         $('aside').removeClass('visible')
         $('#line').removeClass('animate')
     })
+
+    $("#currentYear").text(new Date().getFullYear());
+
+    tippy('#html', {
+        content: "HTML5",
+    });
+
+    tippy('#css', {
+        content: "CSS3",
+    });
+
+    tippy('#js', {
+        content: "JavaScript",
+    });
+
+    tippy('#react', {
+        content: "React",
+    });
+
+    tippy('#figma', {
+        content: "Figma",
+    });
+
+    tippy('#github', {
+        content: "GitHub",
+    });
+
+    tippy('#git', {
+        content: "Git",
+    });
+
+    tippy('#jq', {
+        content: "jQuery",
+    });
+
+    tippy('#vsc', {
+        content: "VS Code",
+    });
 })
